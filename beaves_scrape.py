e0b59816c840dcb92aecd5e26f7889ceb8fa9aa2
Drew S. Ortega <DrewSOrtega@gmail.com>
2019-10-10 00:19:26 -0700

    more work with multithreaded file io

--- beaves_scrape.py
+++ beaves_scrape.py
@@ -9,14 +9,14 @@ from collections import deque
 
 done = False
 
-def write_file(filepath, q):
-    f = open(filepath):
-        while(!done):
-            if(len(q) > 0):
-                f.write(q.pop_left())
-
-
 q = deque()
+def write_file():
+    with open("beaves_out.csv", "w") as f:
+        while(not done):
+            if(q):
+                f.write(q.popleft())
+            
+
 def parse_url(path):
     url = "https://beav.es/{}".format(path)
     title = ""
@@ -29,8 +29,9 @@ def parse_url(path):
         result_url = response.geturl()
     except:
         pass
-    print("{}`{}`{}".format(path, title, result_url))
-
+    line = "{}`{}`{}\n".format(path, title, result_url)
+    q.append(line)
+    print(line)
 if __name__ == '__main__':
     letters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','0','1','2','3','4','5','6','7','8','9']
 
@@ -38,10 +39,10 @@ if __name__ == '__main__':
     perms = list(map(lambda s: 'Z' + s, perms))
     # print("Parsing {} URLs.".format(len(perms)))
  
-    tr = Thread(write_file, args=("beaves_out.csv", q)) 
+    tr = Thread(target=write_file) 
     tr.start()
 
-    pool = mp.Pool(mp.cpu_count())
+    pool = mp.Pool(mp.cpu_count()-1)
 
     pool.map(parse_url, [path for path in perms])
 
